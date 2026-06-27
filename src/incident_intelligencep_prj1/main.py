e2708e574aee8a82194deeb3eee6a6d4426db2c9
aikadoctor-habibshaikh <habib.ai.ka.doctor@gmail.com>
2026-06-28 04:16:14 +0530

    initial merge

---
+++ src/incident_intelligencep_prj1/main.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import sys
+import warnings
+from pathlib import Path
+
+from datetime import datetime
+
+from incident_intelligencep_prj1.crew import IncidentIntelligencePrj1
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    # ensure input/output folders exist
+    input_dir = Path('input') / 'dynatrace_logs'
+    output_dir = Path('output')
+    input_dir.mkdir(parents=True, exist_ok=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    inputs = {
+        'topic': 'AI LLMs',
+        'current_year': str(datetime.now().year),
+        'log_input_path': str(input_dir),
+        'output_path': str(output_dir),
+    }
+
+    try:
+        IncidentIntelligencePrj1().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        'current_year': str(datetime.now().year)
+    }
+    try:
+        IncidentIntelligencePrj1().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        IncidentIntelligencePrj1().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        "current_year": str(datetime.now().year)
+    }
+    
+    try:
+        IncidentIntelligencePrj1().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
